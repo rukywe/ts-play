95885c9ee0b6fdf4eb3a83264ada99d6e2bf43eb
Kelvin Anigboro <rukywe@me.com>
2023-02-09 18:09:00 +0000

    first commit

---
+++ src/models/Author.ts
@@ -0,0 +1,18 @@
+import mongoose, { Document, Schema } from 'mongoose';
+
+export interface IAuthor {
+    name: string;
+}
+
+export interface IAuthorModel extends IAuthor, Document {}
+
+const AuthorSchema: Schema = new Schema(
+    {
+        name: { type: 'string', required: true }
+    },
+    {
+        versionKey: false
+    }
+);
+
+export default mongoose.model<IAuthorModel>('Author', AuthorSchema);
